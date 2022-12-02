9bfdf31225c1afe7d51ad11925b59e67ddecf507
Mitja Bezensek <mitja.bezensek@outfit7.com>
2022-12-02 20:11:02 +0100

    Day 2

--- day1/day1.sh
+++ day1/day1.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-echo "Elf with most calories:"
 paste -sd+ input.txt | sed s/++/\\n/g | bc | sort -nr | head -n1
-
-echo "Cals of top 3 Elves:"
 paste -sd+ input.txt | sed s/++/\\n/g | bc | sort -nr | head -n3 | paste -sd+ | bc
