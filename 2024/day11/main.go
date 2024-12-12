16aed1fc8d8b378ab27f87fd4ac5fb141213101f
Mitja Bezensek <bezensek.mitja@gmail.com>
2024-12-12 22:50:13 +0100

    Day 11

--- 2024/day11/main.go
+++ 2024/day11/main.go
@@ -29,28 +29,48 @@ func input() []int {
 }
 
 func solve(stones []int, times int) int {
+
+	nums := make(map[int]int)
+	for _, s := range stones {
+		nums[s] = 1
+	}
 	for i := 0; i < times; i++ {
-		after := []int{}
-		for _, s := range stones {
+		newNums := make(map[int]int)
+
+		for s, v := range nums {
+			newNums[s] -= v
 			if s == 0 {
-				after = append(after, 1)
+				newNums[1] += v
 				continue
 			}
 			digits := int(math.Log10(float64(s)) + 1)
 			if digits%2 == 0 {
-				after = append(after, s/int(math.Pow10(digits/2)))
-				after = append(after, s%int(math.Pow10(digits/2)))
+				n1 := s / int(math.Pow10(digits/2))
+				n2 := s % int(math.Pow10(digits/2))
+				newNums[n1] += v
+				newNums[n2] += v
 				continue
 			}
-			after = append(after, s*2024)
+			newNums[s*2024] += v
 		}
-		stones = after
+
+		for k, v := range newNums {
+			nums[k] += v
+			if nums[k] <= 0 {
+				delete(nums, k)
+			}
+		}
 	}
-	return len(stones)
+
+	total := 0
+	for _, v := range nums {
+		total += v
+	}
+	return total
 }
 
 func main() {
-	stones := input()
-	fmt.Println(solve(stones, 25))
-	fmt.Println(solve(stones, 75))
+	input := input()
+	fmt.Println(solve(input, 25))
+	fmt.Println(solve(input, 75))
 }
